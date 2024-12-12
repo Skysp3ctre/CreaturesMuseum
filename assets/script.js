6d2a165bde235cf82b832de4331b7858435f185e
Skysp3ctre <s.ponpadung@gmail.com>
2024-12-12 15:41:57 -0500

    men
    men

--- assets/script.js
+++ assets/script.js
@@ -81,3 +81,23 @@ function openModal(type) {
   // Placeholder for modal functionality
   alert(`More info about ${type}`);
 }
+
+// Get the button
+let scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+// When the user scrolls down 200px from the top of the document, show the button
+window.onscroll = function () {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+};
+
+// When the user clicks the button, scroll to the top of the document
+scrollToTopBtn.onclick = function () {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
